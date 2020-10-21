7480123e1e4c15a8269f129fb6bb4c2853d513e1
MCompton96 <matthewcompton49@gmail.com>
2020-10-21 16:20:34 +0100

    added timestamps to comments

--- nc-news/src/components/CommentCard.jsx
+++ nc-news/src/components/CommentCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Votes from './Votes';
+import moment from 'moment';
+import { Link } from '@reach/router';
 
 const CommentCard = (props) => {
-    const { author, body, votes, comment_id, handleDelete} = props;
+    const { author, body, votes, comment_id, handleDelete, created_at} = props;
     return (
     <>
-    <p>Posted by {author}</p>
+    <p>Posted by <Link to={`/${author}/articles`}>{author}</Link> on {moment(created_at).format('LLLL')}</p>
     <p>{body}</p>
     <Votes votes={votes} id={comment_id} comment={true}/>
     <p><button id={comment_id} handleDelete={handleDelete}>Delete Comment</button></p>
