c551c487143d50bf929297bc5d2cc5aa3fbb068e
MCompton96 <matthewcompton49@gmail.com>
2020-10-22 12:31:22 +0100

    css text and article card styling

--- nc-news/src/components/ArticleCard.jsx
+++ nc-news/src/components/ArticleCard.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link } from '@reach/router';
 import Votes from './Votes';
 import moment from 'moment';
+import './ArticleCard.css';
 
 const ArticleCard = (props) => {
     const { article_id, title, body, topic, author, created_at, votes, handleDelete } = props;
     return (
-    <div>
-        <Link to={`/articles/${article_id}`}>{title}</Link>
-    <p>Posted on {moment(created_at).format('LLLL')} by <Link to={`/${author}/articles`}>{author}</Link></p>        
-        <p>{body}</p> 
-        <span><Votes votes={votes} id={article_id} article={true}/></span>
-        <p><button onClick={() => {
+    <div className="article-cards">
+        <span className="container">
+        <Link to={`/articles/${article_id}`} className="title">{title}</Link>
+    <p className="timestamp">Posted on {moment(created_at).format('LLLL')} by <Link to={`/${author}/articles`}>{author}</Link></p>        
+        <p class="body">{body}</p> 
+        </span>
+        <span><Votes votes={votes} id={article_id} article={true} className="votes"/></span>
+        <p class="delete-container"><button onClick={() => {
             handleDelete(article_id)
         }}>Delete Article</button></p>       
     </div>
--- nc-news/src/components/ArticleList.jsx
+++ nc-news/src/components/ArticleList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ArticleCard from './ArticleCard';
+import './ArticleList.css';
 
 const ArticleList = (props) => {
     return (
-        <main>
+        <main className="list">
             {props.articles.map(article => {
                 return (
                     <ArticleCard {...article} key={article.article_id} handleDelete={props.handleDelete} />
